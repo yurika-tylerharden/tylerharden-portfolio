6fc0c1efd5b4475f8695a0a65170d1bf317412be
Tyler Harden <tyler-harden@outlook.com>
2025-04-13 19:46:20 +1000

    Vite being used properly in 4.1

--- tylerharden-portfolio/vite.config.js
+++ tylerharden-portfolio/vite.config.js
@@ -1,6 +1,17 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react()],
-});
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  // server: {
+  //   open: true, // ✅ Auto-open browser on npm run dev
+  // },
+  build: {
+    outDir: 'dist', // ✅ Default build output
+    sourcemap: true, // ✅ Helpful if you ever want better debugging
+  },
+})
